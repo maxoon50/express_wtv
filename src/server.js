43e2139194a15016d88b6b389aa82555fea61083
maxoon <desfouxmaxime@gmail.com>
2018-08-07 15:55:02 +0200

    add filter films server

--- src/server.js
+++ src/server.js
@@ -7,19 +7,31 @@ const fs = require('fs')
 
 const corsOptions = {
   origin: 'http://localhost:8081',
-  optionsSuccessStatus: 200 
+  optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 
-app.get('/ping', function (req, res) {
-  res.send('pong')
+
+
+app.get('/films', function (req, res) {
+  fs.readFile(JSON_FILE, 'utf8', (err, data) => {
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
+    let films = JSON.parse(data);
+    const filmsFiltered = films['films'].map((film) => {
+      return {
+        id: film.id,
+        img: film.img,
+        titre: film.titre
+      }
+    })
+
+    res.send(filmsFiltered)
+  })
 })
 
-app.get('/films', function(req, res){
-  fs.readFile(JSON_FILE, 'utf8', (err, data)=>{
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
-    res.json(JSON.parse(data))
-  })
+app.get('/films/:id', function(req, res){
+  console.log(req.id)
+    res.send(req.id)
 })
 
 app.listen(8005, function () {
