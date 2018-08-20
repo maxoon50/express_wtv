7b7030204fd447baf9c7b5764ab7b27f3c9ceefd
maxoon <desfouxmaxime@gmail.com>
2018-08-20 12:25:42 +0200

    add post

--- src/server.js
+++ src/server.js
@@ -4,13 +4,16 @@ const app = express();
 const cors = require('cors');
 const JSON_FILE = 'src/data/movies.json';
 const fs = require('fs');
+let bodyParser = require('body-parser')
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'http://192.168.1.10:8080','http://localhost:8081', 'http://192.168.1.10:8081'],
+  origin: ['http://localhost:8080', 'http://192.168.1.10:8080','http://localhost:8082', 'http://192.168.1.10:8082','http://localhost:8081', 'http://192.168.1.10:8081',],
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
-
+app.use( express.static('public'));
+app.use(bodyParser.json());
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 app.get('/films', function (req, res) {
@@ -45,6 +48,21 @@ app.get('/films/:id', function(req, res) {
         }
 
 });
+
+app.post('/film',urlencodedParser, function(req, res) {
+  console.log(req.body)
+  fs.readFile(JSON_FILE, 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    let obj = JSON.parse(data);
+    obj.films.push(req.body);
+    let json = JSON.stringify(obj);
+    fs.writeFile(JSON_FILE, json, 'utf8', ()=>{
+      console.log('ok')
+    });
+}});
+})
 
 
 app.listen(8005, function () {
