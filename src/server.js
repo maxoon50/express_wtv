fc8849bf389c0964339940dd5477742b25d6d696
maxoon <desfouxmaxime@gmail.com>
2018-08-22 09:56:08 +0200

    fix cors

--- src/server.js
+++ src/server.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const JSON_FILE = 'src/data/movies.json';
 const fs = require('fs');
 let multer  = require('multer')
 let upload = multer({ dest: 'uploads/' }).single('file')
-
 let bodyParser = require('body-parser')
 
-const corsOptions = {
-  origin: ['http://localhost:8080', 'http://192.168.1.10:8080','http://localhost:8082', 'http://192.168.1.10:8082','http://localhost:8081', 'http://192.168.1.10:8081',],
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
 app.use( express.static('public'));
 app.use(bodyParser.json());
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
+const DOMAIN_NAME = 'http://localhost:8080';
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", DOMAIN_NAME );
+    res.header('Access-Control-Allow-Methods','POST, GET, OPTIONS, PUT');
+    res.header( "Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 
 app.get('/films', function (req, res) {
   fs.readFile(JSON_FILE, 'utf8', (err, data) => {
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
     let films = JSON.parse(data);
     const filmsFiltered = films['films'].map((film) => {
       return {
@@ -53,16 +52,14 @@ app.get('/films/:id', function(req, res) {
 
 app.post('/film', function(req, res) {
     upload(req, res, function (err) {
-        console.log(req.file)
-        let datas = JSON.parse(req.body.datas);
         //datas.titre, datas.resume
         if (err) {
-            res.status(500);
-            res.render('error', { error: err });
-            return res;
+                console.log(err)
+                res.status(500).send( { error: err })
         }else{
-            res.status(200);
-            res.send('ok')
+            console.log(req.file)
+            let datas = JSON.parse(req.body.datas);
+            res.status(200).send('ok');
         }
     })
 })
